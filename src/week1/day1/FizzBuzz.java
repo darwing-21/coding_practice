b917688fa5de8ba506ad715a04e2b82f789f80f6
darwing-21 <darwingarcia2130@gmail.com>
2024-08-16 22:28:59 -0400

    feat(day1): add excersice fizz buzz

--- src/week1/day1/FizzBuzz.java
+++ src/week1/day1/FizzBuzz.java
@@ -1,2 +1,35 @@
-package week1.day1;public class FizzBuzz {
+package week1.day1;
+
+import java.util.ArrayList;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int rangeInitial = 1;
+        int rangeFinal = 15;
+        ArrayList<String> fizzBuzz = fizzBuzz(rangeInitial,rangeFinal);
+        print(fizzBuzz);
+    }
+
+    public static ArrayList<String> fizzBuzz(int i, int j) {
+        ArrayList<String> output = new ArrayList<>();
+        while (i <= j) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                output.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                output.add("Fizz");
+            } else if (i % 5 == 0) {
+                output.add("Buzz");
+            } else {
+                output.add("" + i);
+            }
+            i++;
+        }
+        return output;
+    }
+
+    public static void print(ArrayList<String> fizzBuzz){
+        for (int i =0 ; i<fizzBuzz.size();i++){
+            System.out.println(fizzBuzz.get(i));
+        }
+    }
 }
