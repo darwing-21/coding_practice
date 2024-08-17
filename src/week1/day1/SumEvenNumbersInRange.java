a1a7b04b20db0f54f68f5bdb28e1df1ea9b2bafa
darwing-21 <darwingarcia2130@gmail.com>
2024-08-16 22:28:39 -0400

    feat(day1): add excersice sum even number in range

--- src/week1/day1/SumEvenNumbersInRange.java
+++ src/week1/day1/SumEvenNumbersInRange.java
@@ -1,2 +1,22 @@
-package week1.day1;public class SumEvenNumbersInRange {
+package week1.day1;
+
+public class SumEvenNumbersInRange {
+    public static void main(String[] args) {
+        int rangeInitial = 2;
+        int rangeFinal = 10;
+        int sumEvenRange = sumEvenNumberInRange(rangeInitial,rangeFinal);
+        System.out.println(sumEvenRange);
+    }
+
+    //Solution initial
+    public static int sumEvenNumberInRange(int i, int j) {
+        int output = 0;
+        while (i <= j) {
+            if (i % 2 == 0) {
+                output += i;
+            }
+            i++;
+        }
+        return output;
+    }
 }
