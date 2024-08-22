2d7f95e40be4ead845d445b80ab839f9da659b15
darwing-21 <darwingarcia2130@gmail.com>
2024-08-22 09:53:51 -0400

    feat(day4): add excersice string most large

--- src/week1/day4/LongestStringLength.java
+++ src/week1/day4/LongestStringLength.java
@@ -1,2 +1,21 @@
-package week1.day4;public class LongestStringLength {
+package week1.day4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LongestStringLength {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("apple", "banana", "cherry"));
+        System.out.println(calculateStringMaxLength(list));
+    }
+
+    public static int calculateStringMaxLength(ArrayList<String> list) {
+        int output = -1;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() > output) {
+                output = list.get(i).length();
+            }
+        }
+        return output;
+    }
 }
