23363d3bf48a2f7d678919629b617e78ed369bfd
darwing-21 <darwingarcia2130@gmail.com>
2024-08-22 10:10:25 -0400

    feat(day4): add excersice convert string to upper case

---
+++ src/week1/day4/ConvertStringToUpperCase.java
@@ -0,0 +1,11 @@
+package week1.day4;
+
+public class ConvertStringToUpperCase {
+    public static void main(String[] args){
+        System.out.println(convertUpperCase("hello world"));
+    }
+
+    public static String convertUpperCase(String text){
+        return text.toUpperCase();
+    }
+}
